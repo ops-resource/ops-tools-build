f435f73479e30e78b8ed04bf61c51cad3ca0f917
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-02-07 23:03:23 +1300

    Adding the solution that will hold the custom MsBuild Tasks
    references #2

---
+++ src/ops.tools.build/tasks/Ops.Tools.Build.Tasks/Class1.cs
@@ -0,0 +1,19 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="Ops-Resource">
+// Copyright (c) Ops-Resource. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ops.Tools.Build.Tasks
+{
+    public class Class1
+    {
+    }
+}
